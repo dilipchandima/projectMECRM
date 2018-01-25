223562aa60c19856f5c2a6fb5c02295c80440c93
dilipchandima <dilipchandima@gmail.com>
2018-01-25 08:39:58 +0530

    adding file uploading

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const multer = require('multer');
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const UserController = require('../controllers/user');
 const checkAuth = require('../middleware/check-auth');
 
 router.post("/login", UserController.user_login);
-router.post("/signup", UserController.user_signup);
+router.post("/signup", upload.single('userImage'), UserController.user_signup);
 
 router.get("/getAll", checkAuth, UserController.user_getAll);
 

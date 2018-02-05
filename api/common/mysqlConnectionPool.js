40b82bc91836a12daf5b6abd1cb5b49301b5e3b3
dilipchandima <dilipchandima@gmail.com>
2018-02-05 22:22:17 +0530

    adding changes

--- api/common/mysqlConnectionPool.js
+++ api/common/mysqlConnectionPool.js
@@ -4,7 +4,7 @@ var config = require('./config');
 module.exports = mysql.createPool({
     connectionLimit: 12500,
     host:'localhost',
-    user:'root',
-    password:'root',
-    database:'electric'
+    user:'tidybea1_crm_usr',
+    password:'C&#u$er',
+    database:'tidybea1_me_crm'
 });
